cd21ea15e623840778edc2c5371f540aba3424cb
ayyyowthh <143221114+ayyyowthh@users.noreply.github.com>
2024-12-27 19:06:04 +0100

    Update TB2_Autofarm.lua

--- TB2_Autofarm.lua
+++ TB2_Autofarm.lua
@@ -1,4 +1,1 @@
-local url = "https://raw.githubusercontent.com/ayyyowthh/ScriptTest/refs/heads/main/Test.lua"
-local response = game:GetService("HttpService"):GetAsync(url)
-local scriptFunction = loadstring(response)
-scriptFunction()
+loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/ayyyowthh/ScriptTest/refs/heads/main/Test.lua"))()
